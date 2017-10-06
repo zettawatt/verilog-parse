3690eea6ce21663928311a95e8a6f3de54cb9b43
Chuck McClish <charles.mcclish@microchip.com>
2017-10-05 23:11:12 -0700

    Added more ideas to the README. Added some initial classes

--- verilog-parse.el
+++ verilog-parse.el
@@ -47,8 +47,96 @@
 ;; Required Packages
 (require 'verilog-mode)
 
-(defvar *verilog-parse-database* nil
+(defvar *svlog-database* nil
   "Hash table of all information parsed by 'verparse-parse-source-files'")
+
+;; Base class of all objects
+(defclass svlog-object ()
+  ((buffer
+    :initarg :buffer
+    :initform (error "Must supply a buffer name.")
+    :accessor buffer
+    :documentation "The buffer containing the SV object")
+   (file
+    :initarg :file
+    :initform (error "Must supply a file name.")
+    :accessor file
+    :documentation "The file containing the SV object")))
+
+(defgeneric svlog-goto-defun (object)
+  "Go to the SV object definition or assignment.")
+
+(defgeneric svlog-goto-load (object)
+  "List the SV object instances or loads.")
+
+;; Block type classes
+(defclass svlog-module (svlog-object)
+  ((parent-module
+    :initarg :parent-module
+    :accessor parent-module
+    :documentation "The parent module this module is instantiated in.")
+   (child-modules
+    :initarg :child-modules
+    :accessor child-modules
+    :documentation "Associative array of child modules that are instantiated in this module.")
+   (ports
+    :initarg :ports
+    :accessor ports
+    :documentation "Associative array of all ports of this module."))
+  "SV 'module' object")
+
+(defclass svlog-package (svlog-object)
+  ()
+  "SV 'package' object")
+
+(defclass svlog-interface (svlog-object)
+  ()
+  "SV 'interface' object")
+
+(defclass svlog-class (svlog-object)
+  ()
+  "SV 'class' object")
+
+(defclass svlog-task-function (svlog-object)
+  ()
+  "SV 'function' and 'task' object")
+
+
+;; Signal type classes
+(defclass svlog-input (svlog-object)
+  ()
+  "Module 'input' object")
+
+(defclass svlog-output (svlog-object)
+  ()
+  "Module 'output' object")
+
+(defclass svlog-inout (svlog-input svlog-output)
+  ()
+  "Module 'inout' object")
+
+(defclass svlog-var (svlog-object)
+  ()
+  "Module local variable or wire object")
+
+(defclass svlog-struct (svlog-object)
+  ()
+  "SV 'struct' or 'enum' objects")
+
+
+;; Constant type classes
+(defclass svlog-parameter (svlog-object)
+  ()
+  "SV 'parameter' object")
+
+(defclass svlog-define (svlog-object)
+  ()
+  "SV '`define\' object")
+
+(defclass svlog-constant (svlog-object)
+  ()
+  "SV 'const' or 'localparam' objects")
+
 
 (defun verparse-parse-source-files files
   "Parse all verilog files in FILES and update '*verilog-parse-database*'")
